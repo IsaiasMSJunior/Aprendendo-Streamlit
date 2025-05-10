16df2620d581c2d6affe66b864601b0e58a48d0d
IsaiasMSJunior <isaiasmachadocalculo@gmail.com>
2025-05-10 10:43:18 -0300

    Update aprendendo.py

--- aprendendo.py
+++ aprendendo.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.title("Gerador de documentos PEI")
 st.header("Crie o seus documentos de forma rápida")
 nome = st.text_input("Digite o seu nome")
-st.write("nome")
+st.write(nome)
